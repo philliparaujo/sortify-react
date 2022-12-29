b8c4c3ae1f19f501b92664a73ad3cf677024204f
unknown <phillip@lunenetworks.com>
2022-12-29 15:16:55 -0800

    Shows and links playlists from user

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useApi } from "./spotify.js";
 
 function App() {
   const api = useApi();
+
+  // playlistToggle defaults
+  if (!localStorage.playlistToggle) {
+    localStorage.setItem("playlistToggle", false);
+  }
+  const [playlistToggle, setPlaylistToggle] = useState(
+    JSON.parse(localStorage.playlistToggle)
+  );
+
+  // playlistToggle toggles
+  const togglePlaylists = () => {
+    setPlaylistToggle(!playlistToggle);
+  };
+  useEffect(
+    () => localStorage.setItem("playlistToggle", playlistToggle),
+    [playlistToggle]
+  );
+
+  // API-hidden function extensions
+  const logout = () => {
+    api.logout();
+    if (JSON.parse(localStorage.playlistToggle)) togglePlaylists();
+  };
 
   return (
     <div className="App">
@@ -14,8 +37,22 @@ function App() {
       {!api.isLoggedIn ? (
         <button onClick={api.login}>Login</button>
       ) : (
-        <button onClick={api.logout}>Logout</button>
+        <button onClick={logout}>Logout</button>
       )}
+      {api.id ? (
+        <button onClick={togglePlaylists}>Toggle playlists</button>
+      ) : null}
+      {playlistToggle === true && api.playlists ? (
+        <div>
+          {api.playlists.map((playlist, index) => (
+            <a key={index} href={playlist.external_urls.spotify}>
+              {playlist.name}
+              <br></br>
+            </a>
+          ))}
+          <a href="www.google.com">Hi</a>
+        </div>
+      ) : null}
     </div>
   );
 }
