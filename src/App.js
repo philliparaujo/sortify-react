45be87bd56b2b9b4040d35996bc9fedc64ebb4ec
unknown <phillip@lunenetworks.com>
2022-12-28 20:13:11 -0800

    Finished login system using React hooks

--- src/App.js
+++ src/App.js
@@ -1,118 +1,22 @@
-import React, { useEffect, useState } from "react";
 import "./App.css";
-import { playlists, whoami } from "./spotify";
+import React from "react";
+import { useApi } from "./spotify.js";
 
 function App() {
-  const [myName, setMyName] = useState("????");
-  const [myEmail, setMyEmail] = useState("????");
-  const [allPlaylists, setAllPlaylists] = useState([
-    { name: "a", id: "b", description: "c" },
-    { name: "X", id: "Y", description: "Z" },
-  ]);
-
-  // Create access token
-  const auth_info = {
-    CLIENT_ID: "4e395fc704b74c3dafb22621444b1e64",
-    REDIRECT_URI: "http://localhost:3000",
-    AUTH_ENDPOINT: "https://accounts.spotify.com/authorize",
-    RESPONSE_TYPE: "token",
-  };
-
-  const GetToken = () => {
-    const [token, setToken] = useState("");
-
-    useEffect(() => {
-      const hash = window.location.hash;
-      let token = window.localStorage.getItem("token");
-
-      if (!token && hash) {
-        token = hash
-          .substring(1)
-          .split("&")
-          .find((elem) => elem.startsWith("access_token"))
-          .split("=")[1];
-
-        window.location.hash = "";
-        window.localStorage.setItem("token", token);
-      }
-      setToken(token);
-    }, []);
-
-    return [token, setToken];
-  };
-
-  const [token, setToken] = GetToken();
-
-  // const login = () => {
-  //   // close previous logged-out page
-  //   window.close();
-
-  //   // open new logged-in page
-  //   window.open(
-  //     `${auth_info.AUTH_ENDPOINT}` +
-  //       `?client_id=${auth_info.CLIENT_ID}` +
-  //       `&redirect_uri=${auth_info.REDIRECT_URI}` +
-  //       `&response_type=${auth_info.RESPONSE_TYPE}` +
-  //       `&scope=playlist-read-private user-read-email`,
-  //     "_blank"
-  //   );
-  // };
-
-  const login = () => {
-    // open new logged-in page
-    document.location =
-      `${auth_info.AUTH_ENDPOINT}` +
-      `?client_id=${auth_info.CLIENT_ID}` +
-      `&redirect_uri=${auth_info.REDIRECT_URI}` +
-      `&response_type=${auth_info.RESPONSE_TYPE}` +
-      `&scope=playlist-read-private user-read-email`;
-  };
-
-  const logout = () => {
-    setToken("");
-    window.localStorage.removeItem("token");
-  };
-
-  React.useEffect(() => {
-    if (token) {
-      whoami()
-        .then((result) => {
-          setMyName(result.name);
-          setMyEmail(result.email);
-        })
-        .catch((error) => {
-          if (error.needLogin) {
-            logout();
-            login();
-          }
-        });
-      playlists()
-        .then((p) => setAllPlaylists(p))
-        .catch((error) => console.log("playlists failed", error));
-    }
-  }, [token]);
+  const api = useApi();
 
   return (
     <div className="App">
       <header className="App-header">
-        Hi, {myName} ({myEmail})<h1>Sortify</h1>
-        {!token ? (
-          <div>
-            <button onClick={login}>Login</button>
-            <button disabled>Logout</button>
-          </div>
-        ) : (
-          <div>
-            <button disabled>Login</button>
-            <button onClick={logout}>Logout</button>
-          </div>
-        )}
+        {api.name ? <p>Hi, {api.name}</p> : <p></p>}
+        <h1>Sortify</h1>
       </header>
-      {allPlaylists.map((p) => (
-        <div key={p.id}>{p.name}</div>
-      ))}
+      {!api.isLoggedIn ? (
+        <button onClick={api.login}>Login</button>
+      ) : (
+        <button onClick={api.logout}>Logout</button>
+      )}
     </div>
   );
 }
-
 export default App;
