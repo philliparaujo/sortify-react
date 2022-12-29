2a4b6ec1509927ba21b823f0ced31245a1451a01
unknown <phillip@lunenetworks.com>
2022-12-28 20:13:11 -0800

    Finished login system using React hooks

--- src/spotify.js
+++ src/spotify.js
@@ -1,40 +1,104 @@
-const token = () => localStorage.getItem("token");
+import React from "react";
 
-// Returns a promise that contains JSON object for the /<api> called
-const callApi = (api) =>
-  fetch(`https://api.spotify.com/v1/${api}`, {
+// Log-in info
+//
+const authInfo = {
+  CLIENT_ID: "4e395fc704b74c3dafb22621444b1e64",
+  REDIRECT_URI: "http://localhost:3000",
+  AUTH_ENDPOINT: "https://accounts.spotify.com/authorize",
+  RESPONSE_TYPE: "token",
+  SCOPES: ["user-read-email", "user-library-read"],
+};
+
+const getToken = () => {
+  const hash = window.location.hash;
+  if (!hash) return;
+
+  const token = hash
+    .substring(1)
+    .split("&")
+    .find((elem) => elem.startsWith("access_token"))
+    .split("=")[1];
+
+  window.location.hash = "";
+  return token;
+};
+
+const login = () => {
+  // update URL to spotify login popup
+  window.location =
+    `${authInfo.AUTH_ENDPOINT}` +
+    `?client_id=${authInfo.CLIENT_ID}` +
+    `&redirect_uri=${authInfo.REDIRECT_URI}` +
+    `&response_type=${authInfo.RESPONSE_TYPE}` +
+    `&scope=${authInfo.SCOPES.join(" ")}`;
+};
+
+// Spotify API calls
+//
+const genericAPI = (request, token) => {
+  return fetch(`https://api.spotify.com/v1/${request}`, {
     headers: {
-      Authorization: `Bearer ${token()}`,
+      Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
     },
-  })
-    .then((result) => result.json())
-    .then((result) => {
-      if (result.error) {
-        return Promise.reject({
-          needLogin: true,
-        });
-      }
-      return result;
-    });
+  }).then((result) => result.json());
+};
 
-// Returns a promise that contains a hash with the current user's name and
-// email
-export const whoami = () =>
-  callApi("me").then((result) => ({
-    name: result.display_name,
-    email: result.email,
-  }));
+const getDisplayName = (token) => {
+  return genericAPI("me", token).then((result) => result.display_name);
+};
 
-// Returns a promise that contains an array w/ name, id, and description
-// for every playlist of the current user
-export const playlists = () =>
-  callApi("me/playlists")
-    .then((result) => result.items)
-    .then((items) =>
-      items.map((item) => ({
-        name: item.name,
-        id: item.id,
-        description: item.description,
-      }))
-    );
+const getID = (token) => {
+  return genericAPI("me", token).then((result) => result.id);
+};
+
+const getEmail = (token) => {
+  if (!authInfo.SCOPES.includes("user-read-email")) {
+    throw new Error("No permission to read user email!");
+  }
+  return genericAPI("me", token).then((result) => result.email);
+};
+
+// Information passed to App.js, re-rendered on update
+//
+export function useApi() {
+  const [token, setToken] = React.useState();
+
+  const [name, setName] = React.useState();
+  const [id, setID] = React.useState();
+  const [email, setEmail] = React.useState();
+
+  // prevents repeated getToken calls to be undefined if hash already deleted
+  React.useEffect(() => {
+    const t = getToken();
+    if (t) {
+      setToken(t);
+    }
+  }, []);
+
+  // on token update, updates all related variables
+  React.useEffect(() => {
+    if (token) {
+      // logged in
+      getDisplayName(token).then((name) => setName(name));
+      getID(token).then((id) => setID(id));
+      getEmail(token).then((email) => setEmail(email));
+    } else {
+      // logged out
+      setName(undefined);
+      setID(undefined);
+      setEmail(undefined);
+    }
+  }, [token]);
+
+  // passes information to App.js
+  return {
+    name: name,
+    id: id,
+    email: email,
+    isLoggedIn: token !== undefined,
+    login: login,
+    logout: () => setToken(undefined),
+  };
+}
