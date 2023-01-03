77216ed25ed49c968cefe07f5286180217a0ebfd
unknown <phillip@lunenetworks.com>
2023-01-03 10:06:29 -0800

    Added track backgrounds, bucket adding/deleting/sorting

--- src/track.js
+++ src/track.js
@@ -1,14 +1,17 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 export function Track(props) {
-  const artists = props.track.artists;
-  const duration_ms = props.track.duration_ms;
-  const explicit = props.track.explicit;
+  const divRef = React.useRef();
+  const imgRef = React.useRef();
+
   const id = props.track.id;
   const image = props.track.album.images[0];
   const name = props.track.name;
-  const preview_url = props.track.preview_url;
+  // const artists = props.track.artists;
+  // const durationMs = props.track.duration_ms;
+  // const explicit = props.track.explicit;
+  // const previewUrl = props.track.preview_url;
 
   let imageUrl;
   if (image) {
@@ -17,74 +20,88 @@ export function Track(props) {
     imageUrl = null;
   }
 
-  // console.log(image);
-
-  function getAverageRGB(imgEl) {
-    console.log(imgEl);
-    imgEl.setAttribute("crossOrigin", "");
-
-    var blockSize = 5, // only visit every 5 pixels
-      defaultRGB = { r: 200, g: 0, b: 0 }, // for non-supporting envs
-      canvas = document.createElement("canvas"),
-      context = canvas.getContext && canvas.getContext("2d"),
-      data,
-      width,
-      height,
-      i = -4,
-      length,
-      rgb = { r: 200, g: 0, b: 0 },
-      count = 0;
-    if (!context) {
-      return defaultRGB;
-    }
-
-    console.log(imgEl.naturalHeight, imgEl.offsetHeight, imgEl.height);
-
-    height = canvas.height =
-      imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
-    width = canvas.width =
-      imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
-
-    context.drawImage(imgEl, 0, 0);
-    data = context.getImageData(0, 0, width, height);
-    length = data.data.length;
-
-    while ((i += blockSize * 4) < length) {
-      ++count;
-      rgb.r += data.data[i];
-      rgb.g += data.data[i + 1];
-      rgb.b += data.data[i + 2];
-    }
-
-    // ~~ used to floor values
-    rgb.r = ~~(rgb.r / count);
-    rgb.g = ~~(rgb.g / count);
-    rgb.b = ~~(rgb.b / count);
-
-    return rgb;
-  }
-  /* Set up draggable properties (for sorting)*/
+  /* Colors track on load */
   useEffect(() => {
-    const currentTrack = document.querySelector(`#A${id}`);
-    currentTrack.addEventListener("dragstart", () => {
-      currentTrack.classList.add("dragging");
-    });
-    currentTrack.addEventListener("dragend", () => {
-      currentTrack.classList.remove("dragging");
-    });
+    const getAverageRGB = (img) => {
+      img.setAttribute("crossOrigin", "");
 
-    // console.log(currentTrack.querySelector("img"));
-    // const img = currentTrack.querySelector("img");
-    // const RGB = getAverageRGB(img);
-    // console.log(img, RGB);
-    // currentTrack.style.backgroundColor = `rgb(${RGB.r},${RGB.g},${RGB.b})`;
+      var blockSize = 5, // only visit every 5 pixels
+        defaultRGB = { r: 0, g: 0, b: 0 }, // for non-supporting envs
+        canvas = document.createElement("canvas"),
+        context = canvas.getContext && canvas.getContext("2d"),
+        data,
+        width,
+        height,
+        i = -4,
+        length,
+        rgb = { r: 0, g: 0, b: 0 },
+        count = 0;
+      if (!context) {
+        return defaultRGB;
+      }
+
+      height = canvas.height =
+        img.naturalHeight || img.offsetHeight || img.height;
+      width = canvas.width = img.naturalWidth || img.offsetWidth || img.width;
+
+      context.drawImage(img, 0, 0);
+      data = context.getImageData(0, 0, width, height);
+      length = data.data.length;
+
+      while ((i += 4 * blockSize) < length) {
+        count++;
+        rgb.r += data.data[i];
+        rgb.g += data.data[i + 1];
+        rgb.b += data.data[i + 2];
+      }
+
+      rgb.r /= count;
+      rgb.g /= count;
+      rgb.b /= count;
+
+      return rgb;
+    };
+
+    const setTrackStyle = (img) => {
+      const RGB = getAverageRGB(img.target);
+      divRef.current.style.backgroundColor = `rgb(${RGB.r},${RGB.g},${RGB.b})`;
+
+      const relativeLuminance =
+        0.2126 * RGB.r ** 2.2 + 0.7152 * RGB.g ** 2.2 + 0.0722 * RGB.b ** 2.2;
+
+      divRef.current.children.namedItem("title").style.color = "white";
+      if (relativeLuminance > 100000) {
+        divRef.current.children.namedItem("title").style.color = "black";
+      }
+    };
+
+    const img = imgRef.current;
+    img.onload = (img) => {
+      setTrackStyle(img);
+    };
   }, [id]);
 
   return (
-    <div id={`A${id}`} className="track" draggable>
-      <button>play</button>
-      <img src={imageUrl} alt="F" width="20" height="20"></img>
-      {name}
+    <div
+      ref={divRef}
+      className="track"
+      draggable="true"
+      onDragStart={(e) => e.target.classList.add("dragging")}
+      onDragEnd={(e) => e.target.classList.remove("dragging")}
+    >
+      <button draggable="false">play</button>
+      <img
+        ref={imgRef}
+        src={imageUrl}
+        alt=""
+        width="35"
+        height="35"
+        draggable="false"
+        // hidden
+      ></img>
+      <p id="title" draggable="false">
+        {name}
+      </p>
     </div>
   );
 }
